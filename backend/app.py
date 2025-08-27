3f3310613e4087192125377609211b496b25b14a
Mezefirst <98586830+Mezefirst@users.noreply.github.com>
2025-08-28 00:52:42 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 model = joblib.load("model.pkl")
 scaler = joblib.load("scaler.pkl")
 
