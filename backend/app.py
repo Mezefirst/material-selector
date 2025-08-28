08e02d144eb4649547a6c2fa8993a803bf57e675
Mezefirst <98586830+Mezefirst@users.noreply.github.com>
2025-08-28 02:33:07 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,14 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import pandas as pd
-
+import os
 app = Flask(__name__)
 CORS(app)
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(base_dir, '..', 'ai', 'model.pkl')
+model = joblib.load(model_path)
+
 model = joblib.load("../ai/model.pkl")
 scaler = joblib.load("../ai/scaler.pkl")
 
